fa9fa0e97851691164ec2c3e3e32ed53da24852e
Alexey Shapovalov <alexey.shapovalov@gonitro.com>
2020-05-20 21:06:43 +0100

    Refactored LinkedListTests to common ListTests

---
+++ src/test/java/structures/common/ListTests.java
@@ -0,0 +1,67 @@
+package structures.common;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import structures.interfaces.List;
+
+public abstract class ListTests {
+  public List<Integer> list;
+
+  @BeforeEach
+  void before() {
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(3);
+  }
+
+  @Test
+  void add() {
+    assertEquals(4, list.size());
+    assertEquals(1, list.get(0));
+    assertEquals(2, list.get(1));
+    assertEquals(3, list.get(2));
+    assertEquals(3, list.get(3));
+  }
+
+  @Test
+  void remove() {
+    list.remove(1);
+    list.remove(2);
+    assertEquals(2, list.size());
+    list.remove(3);
+    list.remove(3);
+    assertEquals(0, list.size());
+
+    list.remove(1);
+  }
+
+  @Test
+  void clear() {
+    list.clear();
+    assertEquals(0, list.size());
+  }
+
+  @Test
+  void contains() {
+    assertTrue(list.contains(1));
+    assertTrue(list.contains(2));
+    assertTrue(list.contains(3));
+    assertFalse(list.contains(4));
+
+    list.remove(1);
+    assertFalse(list.contains(1));
+
+    list.clear();
+    assertFalse(list.contains(2));
+
+    list.add(1);
+    assertTrue(list.contains(1));
+  }
+
+  @Test
+  void get() {
+    assertThrows(IndexOutOfBoundsException.class, () -> list.get(100));
+  }
+}
